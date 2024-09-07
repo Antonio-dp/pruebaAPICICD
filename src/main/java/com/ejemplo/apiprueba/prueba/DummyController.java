e1407d41aad5bf8786aa12fa41be7e238b57f62e
Antonio del Pardo <104808960+Antonio-dp@users.noreply.github.com>
2024-09-06 17:09:43 -0700

    Initial Commit

---
+++ src/main/java/com/ejemplo/apiprueba/prueba/DummyController.java
@@ -0,0 +1,16 @@
+package com.ejemplo.apiprueba.prueba;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class DummyController {
+    @GetMapping("/dummy/ping")
+    public ResponseEntity<String> ping(){
+        log.info("GET ping");
+        return ResponseEntity.ok("pong");
+    }
+}
